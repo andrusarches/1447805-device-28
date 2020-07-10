737958c3644104017884c8ee902d3dbb5dd8c5ae
andrusarches <64173881+andrusarches@users.noreply.github.com>
2020-07-10 15:27:16 -0400

    Исправления
    1. Исправлено подключение шрифтов;
    
    2. Исправлены слайдеры;
    
    3. Контент подогнан по Pixel Perfect;
    
    4. Переделана структура шапки сайта;
    
    5.  На главной странице теги "article":   убраны из разделов товаров и заменены на div в слайдере.

--- js/script.js
+++ js/script.js
@@ -4,12 +4,43 @@ var feedbackButton = document.querySelector(".feedback-button");
 var feedbackClose = popupFeedback.querySelector(".pop-up-feedback-close");
 var popupOverlay = document.querySelector(".pop-up-overlay");
 var mapButton = document.querySelector(".contacts-map");
+var feedbackForm = popupFeedback.querySelector("form");
+var feedbackName = popupFeedback.querySelector("[name=feedback-name]");
+var feedbackEmail = popupFeedback.querySelector("[name=feedback-email]");
+var feedbackMessage = popupFeedback.querySelector("[name=feedback-message]");
+
+var isStorageSuportFeedback = true;
+var storageFeedbackName = "";
+var storageFeedbackEmail = "";
+
+try {
+  storageFeedbackName = localStorage.getItem("FeedbackName");
+  storageFeedbackEmail = localStorage.getItem("FeedbackEmail");
+} catch (err) {
+  isStorageSuportFeedback = false;
+}
 
 feedbackButton.addEventListener("click",
   function(evt) {
     evt.preventDefault();
     popupFeedback.classList.add("pop-up-feedback-active");
     popupOverlay.classList.add("pop-up-overlay-active");
+
+    if (storageFeedbackName, storageFeedbackEmail) {
+    feedbackName.value = storageFeedbackName;
+    feedbackEmail.value = storageFeedbackEmail;
+    feedbackMessage.focus();
+    } else {
+    feedbackName.focus();
+  }
+});
+
+feedbackForm.addEventListener("submit", function(evt) {
+
+  if (isStorageSuportFeedback) {
+    localStorage.setItem("FeedbackName", feedbackName.value);
+    localStorage.setItem("FeedbackEmail", feedbackEmail.value);
+  }
 });
 
 feedbackClose.addEventListener("click",
