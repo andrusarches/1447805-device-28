0272864a21aa8492579854ac18c8142ec7c2a2b8
andrusarches <64173881+andrusarches@users.noreply.github.com>
2020-07-11 10:20:22 -0400

    Финальные правки

--- js/popup.js
+++ js/popup.js
@@ -20,13 +20,6 @@ try {
   isStorageSuportFeedback = false;
 }
 
-function initMap() {
-  var device = {lat:55.687031, lng:37.529618};
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 17, center: device});
-  var marker = new google.maps.Marker({position: device, map: map});
-}
-
 feedbackButton.addEventListener("click",
   function(evt) {
     evt.preventDefault();
