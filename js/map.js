3f46d401f4bd2e7cc2e4a6f5d5522c77ac8f74af
andrusarches <64173881+andrusarches@users.noreply.github.com>
2020-07-14 10:21:58 -0400

    Исправления для защиты

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 function initMap() {
   var device = {lat:55.687031, lng:37.529618};
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 17, center: device});
+      document.getElementById("map"), {zoom: 17, center: device});
   var marker = new google.maps.Marker({position: device, map: map});
 }
--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ var storageFeedbackEmail = "";
 function initMap() {
   var device = {lat:55.687031, lng:37.529618};
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 17, center: device});
+      document.getElementById("map"), {zoom: 17, center: device});
   var marker = new google.maps.Marker({position: device, map: map});
 }
 
